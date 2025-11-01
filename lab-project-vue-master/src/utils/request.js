8327c78889d47a85d4020c8607cad0bf99bfa27b
shiqi <149217212+divetostudy@users.noreply.github.com>
2025-11-01 18:49:15 +0800

    Update request.js

--- lab-project-vue-master/src/utils/request.js
+++ lab-project-vue-master/src/utils/request.js
@@ -2,9 +2,9 @@ import axios from "axios"
 
 const request = axios.create({
   //服务器接口
-  // baseURL: 'http://47.121.117.160:8083',
+   baseURL: 'https://a602-backend.zeabur.app:8084',
   //测试接口
-  baseURL: 'http://192.168.203.245:8084',
+  //baseURL: 'http://192.168.203.245:8084',
   timeout: 10000
 })
 
